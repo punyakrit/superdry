9409f337a85903b3146658d55a2ce8a90c3dfbc9
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-23 19:43:01 +0530

    nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
- 
+import { cn } from "@/lib/utils";
+import { NextAuthProvider } from "./Providers";
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
-
+});
 
 export const metadata: Metadata = {
   title: "SuperDry - Your Ultimate Laundry Solution",
-  description: "SuperDry offers hassle-free laundry services with top-notch quality and convenience. Experience the difference and say goodbye to laundry stress!",
-  keywords: "laundry, laundry services, laundry solutions, laundry stress, convenience, quality, SuperDry, laundry delivery, dry cleaning, laundry pickup, laundry care, eco-friendly laundry, laundry app, laundry near me, laundry subscription, professional laundry, laundry experts, laundry expertise, laundry management, laundry maintenance, laundry technology, laundry innovation, laundry efficiency, laundry reliability",
+  description:
+    "SuperDry offers hassle-free laundry services with top-notch quality and convenience. Experience the difference and say goodbye to laundry stress!",
+  keywords:
+    "laundry, laundry services, laundry solutions, laundry stress, convenience, quality, SuperDry, laundry delivery, dry cleaning, laundry pickup, laundry care, eco-friendly laundry, laundry app, laundry near me, laundry subscription, professional laundry, laundry experts, laundry expertise, laundry management, laundry maintenance, laundry technology, laundry innovation, laundry efficiency, laundry reliability",
 };
 
 export default function RootLayout({
@@ -25,14 +27,28 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>{"SuperDry - Your Ultimate Laundry Solution"}</title>
-        <meta name="description" content={"SuperDry offers hassle-free laundry services with top-notch quality and convenience. Experience the difference and say goodbye to laundry stress!"} />
-        <meta name="keywords" content={"laundry, laundry services, laundry solutions, laundry stress, convenience, quality, SuperDry, laundry delivery, dry cleaning, laundry pickup, laundry care, eco-friendly laundry, laundry app, laundry near me, laundry subscription, professional laundry, laundry experts, laundry expertise, laundry management, laundry maintenance, laundry technology, laundry innovation, laundry efficiency, laundry reliability"} />
+        <meta
+          name="description"
+          content={
+            "SuperDry offers hassle-free laundry services with top-notch quality and convenience. Experience the difference and say goodbye to laundry stress!"
+          }
+        />
+        <meta
+          name="keywords"
+          content={
+            "laundry, laundry services, laundry solutions, laundry stress, convenience, quality, SuperDry, laundry delivery, dry cleaning, laundry pickup, laundry care, eco-friendly laundry, laundry app, laundry near me, laundry subscription, professional laundry, laundry experts, laundry expertise, laundry management, laundry maintenance, laundry technology, laundry innovation, laundry efficiency, laundry reliability"
+          }
+        />
         {/* Additional meta tags for SEO can be added here */}
       </head>
-      <body className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}
+      >
+        <NextAuthProvider>{children}</NextAuthProvider>
+      </body>
     </html>
   );
 }
--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,28 +1,66 @@
 "use client";
-import Link from "next/link";
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Appbar() {
   const [menu, setMenu] = useState(false);
   function toggleMenu() {
     setMenu(!menu);
   }
+  const { status, data: session } = useSession();
   return (
     <div>
       <div className="fixed z-30 h-20 backdrop-blur-sm bg-black/40  rounded-b-3xl items-center lg:px-16 px-6 w-full text-white flex justify-between">
         <div className="text-3xl font-bold">SuperDry</div>
+
         <div className="md:hidden">
           <FaBars className=" text-3xl cursor-pointer" onClick={toggleMenu} />
         </div>
+
         <div className="hidden md:flex lg:space-x-12 space-x-8 text-xl font-bold items-center">
           <div className="cursor-pointer">Home</div>
           <div className="cursor-pointer">Contact Us</div>
           <div className="cursor-pointer">Services</div>
-          <div className="cursor-pointer px-7 py-2 border border-white hover:bg-white hover:text-black">
-            Register
-          </div>
+          {status !== "authenticated" ? (
+            <div
+              onClick={() => {
+                signIn("google");
+              }}
+              className="cursor-pointer px-7 py-2 border border-white hover:bg-white hover:text-black"
+            >
+              Register
+            </div>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Image
+                  src={session?.user?.image || ""}
+                  width={40}
+                  height={40}
+                  alt="user-image"
+                  className="cursor-pointer rounded-full border border-white hover:bg-white hover:text-black"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => signOut()}>
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
         </div>
       </div>
 
@@ -45,9 +83,25 @@ function Appbar() {
               <div className="cursor-pointer text-2xl font-semibold transition duration-300">
                 Services
               </div>
-              <div className="cursor-pointer text-2xl font-semibold transition duration-300">
-                Register
-              </div>
+              {status !== "authenticated" ? (
+                <div
+                  onClick={() => {
+                    signIn("google");
+                  }}
+                  className="cursor-pointer text-2xl font-semibold transition duration-300"
+                >
+                  Register
+                </div>
+              ) : (
+                <div
+                  onClick={() => {
+                    signOut();
+                  }}
+                  className="cursor-pointer text-2xl font-semibold transition duration-300"
+                >
+                  Logout
+                </div>
+              )}
             </div>
           </div>
         </div>
