659de86fce9ee33d558af4685bfee2c747cfdde8
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-24 00:25:55 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
-import { NextAuthProvider } from "./Providers";
+import { NextAuthProvider } from "./providers";
 
 const fontSans = FontSans({
   subsets: ["latin"],
---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { SessionProvider } from "next-auth/react";
+
+export const NextAuthProvider = ({children}:any)=>{
+    return (
+        <SessionProvider>
+         {children}
+        </SessionProvider>
+        )
+}
